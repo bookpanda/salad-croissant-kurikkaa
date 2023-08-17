4e2e6081784405c6b0c861b6b21ae27fe6d14b4b
Idhibhat Pankam <marginpankam@gmail.com>
2023-08-17 22:30:46 +0700

    server: config env

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+
+  const configService = app.get(ConfigService);
+  const port = parseInt(configService.get('PORT'));
+  const clientPort = parseInt(configService.get('CLIENT_PORT'));
+
+  app.enableCors({
+    origin: [
+      `http://localhost:${clientPort}`,
+      new RegExp(`^http://192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$`),
+    ],
+  });
+
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
