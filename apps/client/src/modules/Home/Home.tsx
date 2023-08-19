addadfd87675fb1479e9d2df46c56f25117d6896
Idhibhat Pankam <marginpankam@gmail.com>
2023-08-20 00:04:31 +0700

    client: responsive

--- apps/client/src/modules/Home/Home.tsx
+++ apps/client/src/modules/Home/Home.tsx
@@ -4,13 +4,14 @@ import { FC } from "react";
 import { MainPanel } from "./MainPanel";
 import { Header } from "./Header";
 import { Timer } from "./Timer";
+import { Flex } from "@chakra-ui/react";
 
 export const Home: FC = () => {
   return (
-    <>
+    <Flex flexDir="column" alignItems="center">
       <Header />
       <Timer />
       <MainPanel />
-    </>
+    </Flex>
   );
 };
--- apps/client/src/modules/Home/MainPanel/MainPanel.tsx
+++ apps/client/src/modules/Home/MainPanel/MainPanel.tsx
@@ -6,7 +6,10 @@ import { Clicker } from "./Clicker";
 export const MainPanel: FC = () => {
   const { countSalad, countCroissant } = useAppContext();
   return (
-    <Flex w="100%" bgColor="pink" justifyContent={"space-between"}>
+    <Flex
+      w={{ base: "100%", lg: "70%", xl: "50%" }}
+      justifyContent={"space-between"}
+    >
       <Clicker item="salad" count={countSalad} />
       <Clicker item="croissant" count={countCroissant} />
     </Flex>
