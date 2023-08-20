c73d53dd4ee02e4ab3b13a1fbe5a7b1745a92d39
Idhibhat Pankam <marginpankam@gmail.com>
2023-08-20 23:53:54 +0700

    server: connect cron job

--- apps/server/src/results/results.service.ts
+++ apps/server/src/results/results.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
-export class ResultsService {}
+export class ResultsService {
+  private readonly logger = new Logger(ResultsService.name);
+
+  //   @Cron('0 0 * * *')
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  resetScore() {
+    this.logger.log('Resetting score');
+  }
+}
